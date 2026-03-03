0f8352f9022f58a40f91984f490e12ba3bb97c9c
Juliana Novoa <julianansolano@gmail.com>
2026-03-03 15:35:29 -0500

    Dependencias necesarias

--- src/main/java/com/example/proyecto/thymeleaf/model/entity/UsuarioAdministrador.java
+++ src/main/java/com/example/proyecto/thymeleaf/model/entity/UsuarioAdministrador.java
@@ -1,5 +1,24 @@
 package com.example.proyecto.thymeleaf.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLRestriction("status = 0")
+@SQLDelete(sql = "UPDATE application SET status = 1 WHERE id=?")
 public class UsuarioAdministrador {
-    
+
+   
 }
